393518dc9e4fd300f4c3341b068074d250381c36
devarogundade <devarogundade@gmail.com>
2023-09-17 17:51:55 +0100

    update

--- homepage/src/views/HomeView.vue
+++ homepage/src/views/HomeView.vue
@@ -139,7 +139,7 @@
                   </div>
                 </td>
                 <td>
-                  <p class="service_uses">Enables you to access any external data source like wheather forecast, or sport prediction on-chain.</p>
+                  <p class="service_uses">Enables you to access external data sources like weather forecast, and sport prediction on chain.</p>
                 </td>
                 <td>
                   <div class="service_provider">
